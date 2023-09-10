fac2616f093ae1ac1b7d88cd421eb58bd9a07277
dokyun-kim4 <donny5557@gmail.com>
2023-09-10 19:50:22 -0400

    Add angle tolerance for rotation

--- warmup_project/drive_square.py
+++ warmup_project/drive_square.py
@@ -23,11 +23,11 @@ class DriveSquare(Node):
         self.ang_vel = 0.0
 
         self.prev_coord = [0,0] # (x,y)
-        self.goal_angle = 90
+        self.goal_angle = 88 # Using 89 instead of 90 for tolerance during rotation
+        self.line_count = 0
 
         self.lin_goal_reached = False
         self.done = False
-
 
     def get_angle(self,w):
         '''
@@ -37,32 +37,32 @@ class DriveSquare(Node):
 
     def square_draw(self,msg):
         '''
-        Calculate lin_vel & ang_vel according to current position
+        Determine when to move forward, turn, or stop.
         '''
         crnt_coord = [msg.pose.pose.position.x, msg.pose.pose.position.y]
         crnt_ang = self.get_angle(msg.pose.pose.orientation.w)
         
-        if self.done:
+        if self.line_count == 4: # Stop when 4 lines have been drawn
             self.lin_vel = 0.0
             self.ang_vel = 0.0
 
-        elif dist(self.prev_coord,crnt_coord) <= 1 and not self.lin_goal_reached:
+        elif dist(self.prev_coord,crnt_coord) <= 1 and not self.lin_goal_reached: # Move forward until 1 meter reached
             self.lin_vel = 0.2
         
-        else: # If 1 meter reached
+        else: # Start turning when 1 meter reached
             self.lin_goal_reached = True
-            self.lin_vel = 0.0 # Stop
+            self.lin_vel = 0.0 # Stop moving forward
             self.prev_coord = crnt_coord # Update prev_coord to crnt_coord
             # Turn until goal angle
             if crnt_ang <= self.goal_angle:
+                print(f"goal: {self.goal_angle}, current: {crnt_ang}")
                 self.lin_vel = 0.0
-                self.ang_vel = 0.15
+                self.ang_vel = 0.5
             else: # If goal angle reached
-                print("I turned")
                 self.ang_vel = 0.0 # Stop turning
-                self.goal_angle += 90
-                print(f'Next angle goal: {self.goal_angle}')
+                self.goal_angle += 90 # Update goal_angle
                 self.lin_goal_reached = False
+                self.line_count += 1
 
     def run_loop(self):
         '''
