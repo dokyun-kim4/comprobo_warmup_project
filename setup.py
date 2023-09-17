db1e53e35811ee9fe87caf3a8886264ba4efed62
dokyun-kim4 <donny5557@gmail.com>
2023-09-17 15:50:09 -0400

    Add entry point for `finite_state_controller.py`

--- setup.py
+++ setup.py
@@ -25,7 +25,8 @@ setup(
             'drive_square = warmup_project.drive_square:main',
             'wall_follower = warmup_project.wall_follower:main',
             'person_follower = warmup_project.person_follower:main',
-            'object_avoider = warmup_project.object_avoider:main'
+            'object_avoider = warmup_project.object_avoider:main',
+            'finite_state_controller = warmup_project.finite_state_controller:main'
         ],
     },
 )
