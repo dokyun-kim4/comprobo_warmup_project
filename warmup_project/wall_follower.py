bef88be340eef52856bd3b834ecd142d6d53ee18
dokyun-kim4 <donny5557@gmail.com>
2023-09-17 22:27:58 -0400

    Visualization of wall using markers

--- warmup_project/wall_follower.py
+++ warmup_project/wall_follower.py
@@ -1,8 +1,10 @@
 import rclpy
 from math import dist,sqrt
+from numpy import cos,sin, deg2rad
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from visualization_msgs.msg import Marker, MarkerArray
 
 
 class WallFollow(Node):
@@ -11,9 +13,15 @@ class WallFollow(Node):
 
         # subcribe to odometry node, calculate when to move & turn
         self.laser_sub = self.create_subscription(LaserScan,'scan',callback=self.get_data,qos_profile=10)
+    
 
         # publish the lin_vel and ang_vel
         self.vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.marker_pub1 = self.create_publisher(Marker,'visualization_marker1',10)
+        self.marker_pub2 = self.create_publisher(Marker,'visualization_marker2',10)
+        self.marker_pub3 = self.create_publisher(Marker,'visualization_marker3',10)
+
+
         timer_period = 0.1 # seconds
         self.timer = self.create_timer(timer_period, callback = self.run_loop)
         
@@ -23,13 +31,20 @@ class WallFollow(Node):
 
         self.fixing_angle = False
 
+
+    def pol2cart(self,rho, phi):
+        x = rho * cos(deg2rad(phi))
+        y = rho * sin(deg2rad(phi))
+        return(x, y)       
+
+
     def get_data(self,msg):
         '''
         Store neato's distance from wall @ 45,135 degrees and an approximation error value
-        '''
-        
+        '''        
         self.dists = {'deg45':msg.ranges[45],'deg90':msg.ranges[90],'deg135':msg.ranges[135]}
         self.dists['pcnt_error'] = (self.dists['deg45']-sqrt(2)*self.dists['deg90'])/(sqrt(2)*self.dists['deg90'])
+        print(self.dists)
 
     def check_angle(self):
         '''
@@ -41,13 +56,47 @@ class WallFollow(Node):
         '''
         Publish the message
         '''
+
         msg = Twist()
+        if self.dists:
+            keys = [45,90,135]
+            markerlist = []
+            for key in keys:
+                marker = Marker()
+                dist = self.dists["deg"+str(key)]
+                x,y = self.pol2cart(dist,key)
+                marker.header.frame_id = "base_link"
+                marker.ns = "my_namespace"
+                marker.id = 0
+                marker.type = Marker.SPHERE
+                marker.action = Marker.MODIFY
+                marker.pose.position.x = x
+                marker.pose.position.y = y
+                marker.pose.position.z = 0.0
+                marker.pose.orientation.x = 0.0
+                marker.pose.orientation.y = 0.0
+                marker.pose.orientation.z = 0.0
+                marker.pose.orientation.w = 1.0
+                marker.scale.x = 0.2
+                marker.scale.y = 0.2
+                marker.scale.z = 0.2
+                marker.color.a = 1.0; # Don't forget to set the alpha!
+                marker.color.r = 255.0
+                marker.color.g = 0.0
+                marker.color.b = 0.0
+                markerlist.append(marker)
+
+        
+            self.marker_pub1.publish(markerlist[0])
+            self.marker_pub2.publish(markerlist[1])
+            self.marker_pub3.publish(markerlist[2])
+                
         k = 0.4
         
         if self.dists:
             if self.check_angle():
                 msg.angular.z = 0.0
-                msg.linear.x = 0.2
+                msg.linear.x = 0.05
 
             else:
                 msg.linear.x = 0.0
